78d033b5aa4e61e35e05df82f1fd06f2f7d083be
chadanlo <tiago.quin@gmail.com>
2019-12-14 20:33:32 +0100

    add Task

--- labo3/task/task.go
+++ labo3/task/task.go
@@ -1,1 +1,37 @@
 package task
+
+/**
+ * Interface wanted for the Network
+ */
+type Network interface {
+	EmitEcho(id uint16) bool
+}
+
+type Manager interface {
+	GetElected() uint16
+}
+
+/**
+ * Represents an applicative Task
+ */
+type Task struct {
+	currentElected uint16
+	shouldRunElection bool
+	m Manager
+	n Network
+}
+
+func (t *Task) Run() {
+	t.shouldRunElection = true
+
+	for {
+		if t.shouldRunElection {
+			t.currentElected = t.m.GetElected()
+			t.shouldRunElection = false
+		} else {
+			if t.n.EmitEcho(t.currentElected) == false {
+				t.shouldRunElection = true
+			}
+		}
+	}
+}
