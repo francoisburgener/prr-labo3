a105085a69c1ef889494648478e71e7615593f2e
chadanlo <tiago.quin@gmail.com>
2019-12-15 14:37:25 +0100

    add runelection at start

--- labo3/task/task.go
+++ labo3/task/task.go
@@ -33,15 +33,17 @@ func (t *Task) Run(manager Manager, network Network) {
 	t.n = network
 	t.shouldRunElection = true
 
+	t.m.RunElection()
+
 	for {
 
 		log.Println("Task : get the elected processus")
 		t.currentElected = t.m.GetElected()
-		time.Sleep(time.Second * 1)
 		hasAnswered := t.n.EmitEcho(t.currentElected)
 		if !hasAnswered {
 			t.m.RunElection()
 		}
+		time.Sleep(time.Second * 1)
 	}
 /*
 	for {
